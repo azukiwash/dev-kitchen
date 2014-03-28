f72660c650cb4a20a220202d73f2ca9037eaf06f
azukiwash <azuki.washing.machine@gmail.com>
2014-03-28 20:42:19 +0900

    add packages for rbenv and pyenv

--- site-cookbooks/dev-tools/recipes/default.rb
+++ site-cookbooks/dev-tools/recipes/default.rb
@@ -22,6 +22,75 @@ include_recipe "zsh"
   end
 end
 
+# for build rbenv
+case node[:platform]
+when "centos"
+  %w[
+    gcc-c++
+    glibc-headers
+    openssl-devel
+    readline
+    libyaml-devel
+    readline-devel
+    zlib
+    zlib-devel
+  ].each do |pkg|
+    package pkg do
+      action :upgrade
+    end
+  end
+when "ubuntu"
+  %w[
+    autoconf
+    bison
+    build-essential
+    libssl-dev
+    libyaml-dev
+    libreadline6
+    libreadline6-dev
+    zlib1g
+    zlib1g-dev
+  ].each do |pkg|
+    package pkg do
+      action :upgrade
+    end
+  end
+end
+
+# for build pyenv
+case node[:platform]
+when "centos"
+  %w[
+    zlib-devel
+    bzip2
+    bzip2-devel
+    readline-devel
+    sqlite
+    sqlite-devel
+  ].each do |pkg|
+    package pkg do
+      action :upgrade
+    end
+  end
+when "ubuntu"
+  %w[
+    build-essential
+    libssl-dev
+    zlib1g-dev
+    libbz2-dev
+    libreadline-dev
+    libsqlite3-dev
+    wget
+    curl
+    llvm
+  ].each do |pkg|
+    package pkg do
+      action :upgrade
+    end
+  end
+end
+
+
 package "netcat" do
   case node[:platform]
   when "centos"
